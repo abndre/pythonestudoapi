291bdb2f1d66893444871c7bbc62558d9677bbc4
Andre Santos Barros da Silva <andrezio@live.com>
2020-05-22 12:29:25 -0300

    update endpoints

--- app/app.py
+++ app/app.py
@@ -1,10 +1,22 @@
 from flask import Flask, escape, request
+from flask_restplus import abort
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     return 'Hello World'
 
+@app.route('/breakfunc')
+def breakfunc():
+    dead()
+
+def dead():
+    abort(403, message="hAAAAAAAAAAAAAAA")
+
+@app.route('/calc/<int:n_prim>')
+def cal_prim(n_prim):
+    return "Prim {}".format(n_prim)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
